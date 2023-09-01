8fd9e3ec8529954df2b9751038476e0baca7f6d3
Manoj Kumar <71879662+manojxk@users.noreply.github.com>
2023-09-01 18:54:12 +0530

    Update Python.py

--- Python.py
+++ Python.py
@@ -2,39 +2,36 @@ import os
 import re
 import pandas as pd
 
+# Create an empty DataFrame with the desired column names
+output_df = pd.DataFrame(columns=["Microservice", "Tag", "PROD1", "JPPROD2"])
+
 def append_to_excel(microservice, tag, rlm_ids):
-    # Check if the Excel file exists
-    file_exists = os.path.isfile("output_data.xlsx")
+    global output_df  # Access the global DataFrame
 
     # Extract the PROD1 and JPPROD2 IDs using regular expressions
     pattern = r"(\w+)\s*-\s*https://releaseorchestrationdeployment\.citigroup\.net/brpm/requests/(\d+)"
     matches = re.findall(pattern, rlm_ids, re.IGNORECASE)
 
-    # Create a DataFrame with the extracted data
-    df = pd.DataFrame(matches, columns=["Name", "ID"])
-    
-    # Add the microservice and tag values
-    df["Microservice"] = microservice
-    df["Tag"] = tag
+    # Create a dictionary with the data to append
+    new_row = {
+        "Microservice": microservice,
+        "Tag": tag,
+        "PROD1": None,  # Initialize with None
+        "JPPROD2": None  # Initialize with None
+    }
 
-    if not file_exists:
-        # If the file doesn't exist, create it with column names
-        df.to_excel("output_data.xlsx", index=False)
-        print("Excel file 'output_data.xlsx' created with column names.")
-    else:
-        # If the file exists, load existing data
-        existing_df = pd.read_excel("output_data.xlsx")
-        
-        # Check if the existing DataFrame is empty
-        if existing_df.empty:
-            # If empty, simply save the current DataFrame
-            df.to_excel("output_data.xlsx", index=False)
-            print("Data appended to 'output_data.xlsx' in a new row.")
-        else:
-            # If not empty, concatenate the current DataFrame horizontally
-            updated_df = pd.concat([existing_df, df], axis=1)
-            updated_df.to_excel("output_data.xlsx", index=False)
-            print("Data appended to 'output_data.xlsx' in a new row.")
+    # Fill in the PROD1 and JPPROD2 values if they exist in the matches
+    for name, id in matches:
+        if name == "PROD1":
+            new_row["PROD1"] = id
+        elif name == "JPPROD2":
+            new_row["JPPROD2"] = id
+
+    # Append the new row to the DataFrame
+    output_df = output_df.append(new_row, ignore_index=True)
+
+    # Print the updated DataFrame
+    print(output_df)
 
 # Example usage:
 microservice = "169068-auth-d-partyauth-apct-dgl-ea"
@@ -42,4 +39,6 @@ tag = "jenkins-169068.api.v2.auth.d.partyauth.apct.dgl.ea.gc.iut.int-29"
 rlm_ids = "PROD1 - https://releaseorchestrationdeployment.citigroup.net/brpm/requests/7430862, JPPROD2 - https://releaseorchestrationdeployment.citigroup.net/brpm/requests/7430865"
 append_to_excel(microservice, tag, rlm_ids)
 
-# You can call append_to_excel function with different values for microservice, tag, and rlm_ids to append data to the Excel file in a single row.
+# Save the DataFrame to an Excel file
+output_df.to_excel("output_data.xlsx", index=False)
+print("Data saved to 'output_data.xlsx'.")
