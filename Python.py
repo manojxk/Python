6f9d17a9f53d1d818cfaeb85c72a1e1fe910c45a
Manoj Kumar <71879662+manojxk@users.noreply.github.com>
2023-09-04 20:56:15 +0530

    Update Python.py

--- Python.py
+++ Python.py
@@ -1,50 +1,55 @@
-import os
-import re
+import requests
 import pandas as pd
+from requests.auth import HTTPBasicAuth
 
-# Check if the Excel file exists
-file_exists = os.path.isfile("output_data.xlsx")
+# Function to get the Jenkins URL based on microservice name
+def get_jenkins_url(microservice_name):
+    parts = microservice_name.split("-")
+    prefix = parts[0]
+    rest = parts[1:]
+    jenkins_url = f"{prefix}.api.v2.{'.'.join(rest)}.gc.iut.int"
+    return jenkins_url
 
-# Create an empty DataFrame with the desired column names
-if not file_exists:
-    output_df = pd.DataFrame(columns=["Microservice", "Tag", "PROD1", "JPPROD2"])
-else:
-    output_df = pd.read_excel("output_data.xlsx")
+# Function to get the latest Jenkins build information
+def get_latest_jenkins_build(url, username, password):
+    try:
+        response = requests.get(url, auth=HTTPBasicAuth(username, password))
+        response.raise_for_status()  # Raise an exception for HTTP errors
+        data = response.json()
+        return data.get('lastSuccessfulBuild')
 
-def append_to_excel(microservice, tag, rlm_ids):
-    global output_df  # Access the global DataFrame
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+        return None
 
-    # Extract the PROD1 and JPPROD2 IDs using regular expressions
-    pattern = r"(\w+)\s*-\s*https://releaseorchestrationdeployment\.citigroup\.net/brpm/requests/(\d+)"
-    matches = re.findall(pattern, rlm_ids, re.IGNORECASE)
+# Function to fetch and save the latest builds to Excel
+def get_latest_builds_to_excel(region, component_names, output_filename, username, password):
+    jenkins_base_url = f"https://ebjenkins02.nam.nsroot.net/job/{region}/job/169068/job/consumer_api_uat/job"
 
-    # Create a dictionary with the data to append
-    new_row = {
-        "Microservice": microservice,
-        "Tag": tag,
-        "PROD1": None,  # Initialize with None
-        "JPPROD2": None  # Initialize with None
-    }
+    build_data = []
+    for component_name in component_names:
+        component_url = f"{jenkins_base_url}/{component_name}/api/json"
+        latest_build_info = get_latest_jenkins_build(component_url, username, password)
 
-    # Fill in the PROD1 and JPPROD2 values if they exist in the matches
-    for name, id in matches:
-        if name == "PROD1":
-            new_row["PROD1"] = id
-        elif name == "JPPROD2":
-            new_row["JPPROD2"] = id
+        if latest_build_info:
+            build_data.append({
+                "Component": component_name,
+                "Build Number": latest_build_info['number'],
+                "Build URL": latest_build_info['url']
+            })
 
-    # Append the new row to the DataFrame
-    output_df = pd.concat([output_df, pd.DataFrame([new_row])], ignore_index=True)
+    if build_data:
+        df = pd.DataFrame(build_data)
+        df.to_excel(output_filename, index=False)
+        print(f"Build information saved to {output_filename}")
+    else:
+        print("No valid build information found.")
 
-    # Print the updated DataFrame
-    print(output_df)
+# Example usage
+jenkins_username = "ms59214"
+jenkins_password = "Sunita@9791"
+region = "APAC"
+component_names = [get_jenkins_url("169068-aim-d-publicrefdatamgt-ob-ea")]
+output_filename = "latest_builds.xlsx"
 
-# Example usage:
-microservice = "169068-auth-d-partyauth-apct-dgl-ea"
-tag = "jenkins-169068.api.v2.auth.d.partyauth.apct.dgl.ea.gc.iut.int-29"
-rlm_ids = "PROD1 - https://releaseorchestrationdeployment.citigroup.net/brpm/requests/7430862, JPPROD2 - https://releaseorchestrationdeployment.citigroup.net/brpm/requests/7430865"
-append_to_excel(microservice, tag, rlm_ids)
-
-# Save the DataFrame to an Excel file
-output_df.to_excel("output_data.xlsx", index=False)
-print("Data saved to 'output_data.xlsx'.")
+get_latest_builds_to_excel(region, component_names, output_filename, jenkins_username, jenkins_password)
