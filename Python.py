cff0d1af8b2a80e85ec7c719d5782ba85790133d
Manoj Kumar <71879662+manojxk@users.noreply.github.com>
2023-09-05 22:08:12 +0530

    Update Python.py

--- Python.py
+++ Python.py
@@ -1,67 +1,126 @@
-import pandas as pd
 import requests
-from requests.auth import HTTPBasicAuth
+import json
+import yaml
 
-# Function to get the Jenkins URL based on microservice name
-def get_jenkins_url(microservice_name):
-    parts = microservice_name.split("-")
-    prefix = parts[0]
-    rest = parts[1:]
-    jenkins_url = f"{prefix}.api.v2.{'.'.join(rest)}.gc.iut.int"
-    return jenkins_url
+# Define common headers
+headers = {
+    "Content-Type": "application/json"
+}
 
-# Function to get the latest Jenkins build information
-def get_latest_jenkins_build(url, username, password):
+# Define your Bitbucket username and password
+username = "Your_Bitbucket_Username"
+password = "Your_Bitbucket_Password"
+
+# Function to make an authorized HTTP GET request with username and password
+def make_authorized_http_request(url):
     try:
-        response = requests.get(url, auth=HTTPBasicAuth(username, password))
-        response.raise_for_status()  # Raise an exception for HTTP errors
-        data = response.json()
-        return data.get('lastSuccessfulBuild')
-
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
+        response = requests.get(url, headers=headers, auth=(username, password))
+        response.raise_for_status()
+        return response.json()
+    except Exception as e:
+        print(f"HTTP Request error: {e}")
         return None
 
-# Function to process the Excel file and get latest builds
-def process_excel(input_filename, output_filename, jenkins_username, jenkins_password):
+# Function to authenticate with Bitbucket API
+def authenticate():
+    base_url = "https://cedt-gct-bitbucket.nam.nsroot.net/bitbucket/projects/CONSUMERAPI"
     try:
-        # Read the Excel file
-        df = pd.read_excel(input_filename)
+        response = make_authorized_http_request(base_url)
+        if response is not None:
+            print(response)
+    except Exception as e:
+        print(f"Authentication error: {e}")
 
-        # Ensure that the "Microservice Name" column exists in the Excel file
-        if "Microservice Name" not in df.columns:
-            raise ValueError("Column 'Microservice Name' not found in the Excel file.")
+# Function to get inbound URI mappings
+def get_inbound_uris(region, service_name):
+    print("INBOUND REQUEST")
+    bb_url = (
+        f"https://cedt-gct-bitbucket.nam.nsroot.net/bitbucket/projects/CONSUMERAPI/{region}/{service_name}-config/browse/"
+        f"{service_name}-config/UAT2/{service_name.replace('-', '')}/{service_name.replace('-', '')}.yml"
+    )
 
-        # Create an empty list to store build data
-        build_data = []
+    uri_mapping_list = []
 
-        # Iterate through the microservice names in the Excel file
-        for microservice_name in df["Microservice Name"]:
-            jenkins_url = get_jenkins_url(microservice_name)
-            latest_build_info = get_latest_jenkins_build(jenkins_url, jenkins_username, jenkins_password)
+    try:
+        response_text = make_authorized_http_request(bb_url)
+        if response_text:
+            component_config = yaml.safe_load(response_text)
 
-            if latest_build_info:
-                build_data.append({
-                    "Microservice Name": microservice_name,
-                    "Build Number": latest_build_info['number'],
-                    "Build URL": latest_build_info['url']
-                })
-
-        # Create a DataFrame from the build data and save it to Excel
-        if build_data:
-            build_df = pd.DataFrame(build_data)
-            build_df.to_excel(output_filename, index=False)
-            print(f"Build information saved to {output_filename}")
-        else:
-            print("No valid build information found.")
+            # You can parse the YAML data and populate uri_mapping_list here
 
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(f"Error from BB :: {bb_url}")
+        # Handle the error and populate uri_mapping_list accordingly
 
-# Example usage
-input_filename = "microservices.xlsx"  # Replace with your Excel file path
-output_filename = "latest_builds.xlsx"
-jenkins_username = "ms59214"
-jenkins_password = "Sunita@9791"
+    return uri_mapping_list
 
-process_excel(input_filename, output_filename, jenkins_username, jenkins_password)
+# Function to get outbound APIs
+def get_outbound_apis(region, service_name, domain):
+    print("OUTBOUND REQUEST")
+    bb_url = (
+        f"https://cedt-gct-bitbucket.nam.nsroot.net/bitbucket/projects/CONSUMERAPI/{region}/{service_name}/browse/"
+        f"{service_name}/src/main/resources/application.yml"
+    )
+
+    out_bound_list = []
+
+    try:
+        response_text = make_authorized_http_request(bb_url)
+        if response_text:
+            component_config = yaml.safe_load(response_text)
+
+            # You can parse the YAML data and populate out_bound_list here
+
+    except Exception as e:
+        print(f"Error from BB :: {bb_url}")
+        # Handle the error and populate out_bound_list accordingly
+
+    return out_bound_list
+
+# Function to get Jenkins configuration
+def get_jenkins_config(region, service_name):
+    print("JENKINS REQUEST")
+    bb_url = f"https://cedt-gct-bitbucket.nam.nsroot.net/bitbucket/projects/CONSUMERAPI/{region}/{service_name}/browse/"
+    jenkins_url = {}
+
+    try:
+        response_text = make_authorized_http_request(bb_url)
+        if response_text:
+            # Process the response_text and populate jenkins_url dictionary
+
+    except Exception as e:
+        print(f"ERROR IN JENKIN URL :: {service_name}")
+        # Handle the error and populate jenkins_url accordingly
+
+    return jenkins_url
+
+# Function to get active profiles
+def get_active_profiles(region, service_name):
+    bb_url = (
+        f"https://cedt-gct-bitbucket.nam.nsroot.net/bitbucket/projects/CONSUMERAPI/{region}/{service_name}/browse/"
+        f"{service_name}/src/main/resources/bootstrap.yml"
+    )
+
+    active_profiles = service_name
+    try:
+        response_text = make_authorized_http_request(bb_url)
+        if response_text:
+            component_config = yaml.safe_load(response_text)
+            active_profiles = f"{service_name},{component_config.get('spring', {}).get('profiles', {}).get('active', 'NA')}"
+    except Exception as e:
+        print(f"Error from BB :: {bb_url}")
+        # Handle the error and set active_profiles accordingly
+
+    return active_profiles
+
+# Example usage:
+if __name__ == "__main__":
+    authenticate()
+    inbound_uris = get_inbound_uris("example_region", "example_service")
+    print("Inbound URIs:", inbound_uris)
+    outbound_apis = get_outbound_apis("example_region", "example_service", "example_domain")
+    print("Outbound APIs:", outbound_apis)
+    jenkins_config = get_jenkins_config("example_region", "example_service")
+    print("Jenkins Config:", jenkins_config)
+    active_profiles = get_active_profiles("example_region", "example_service")
+    print("Active Profiles:", active_profiles)
