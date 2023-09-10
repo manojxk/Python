a8e0439b43c39aa474c5d1c94e5e6dcb5e7d45ee
Manoj Kumar <71879662+manojxk@users.noreply.github.com>
2023-09-10 17:51:21 +0530

    Update Python.py

--- Python.py
+++ Python.py
@@ -1,7 +1,23 @@
-# Start Request
-start_request_button = wait.until(EC.presence_of_element_located((By.XPATH, "//a[@data-method='put' and @rel='nofollow']/img[@alt='Btn-start-request']/parent::a")))
-start_request_button.click()
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-# Click the green tick
-complete_button = wait.until(EC.presence_of_element_located((By.XPATH, "//input[@name='complete' and @title='Complete' and @type='image' and @alt='Complete']")))
-complete_button.click()
+try:
+    wait = WebDriverWait(driver, 600)
+    element = wait.until(EC.text_to_be_present_in_element(
+        (By.XPATH, "(//*[@class='state completeRequestStep'])[10]"), "Complete"))
+    
+    # Perform actions after "Complete" text is present
+    promote_button = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "td[title='Promote to Next Environment']")))
+    promote_button.click()
+    
+    st_automation_button = wait.until(EC.presence_of_element_located((By.ID, "st_automation")))
+    st_automation_button.click()
+    
+    rlm = wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id='argument_10243']")))
+    append_to_excel(microservice, jenkins_tag_name, rlm.text)
+
+except Exception as e:
+    print(f"Error: {e}")
+finally:
+    driver.quit()  # Close the driver when done
