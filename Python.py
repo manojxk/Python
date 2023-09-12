5991ba210098a5de20d7bc00a3f802730abb223c
Manoj Kumar <71879662+manojxk@users.noreply.github.com>
2023-09-12 21:23:57 +0530

    Update Python.py

--- Python.py
+++ Python.py
@@ -1,59 +1,80 @@
 import requests
 import logging
 
-def close_open_pull_requests():
+def create_pull_request(username, password, project, repo_name, base_branch, compare_branch, reviewers):
     # Configure logging
-    logging.basicConfig(filename='pull_request_closure.log', level=logging.INFO,
+    logging.basicConfig(filename='pull_request_creation.log', level=logging.INFO,
                         format='%(asctime)s - %(levelname)s - %(message)s')
-
-    # Replace with your Bitbucket credentials and repository information
-    username = 'ss21454'
-    password = 'HanaHamdan@786'
 
     # Authenticate with Bitbucket API
     auth = (username, password)
-    headers = {"X-Atlassian-Token": "no-check"}
+    headers = {"Content-Type": "application/json"}
 
-    # Get a list of your open pull requests
-    url = 'https://cedt-gct-bitbucket.nam.nsroot.net/bitbucket/rest/api/1.0/dashboard/pull-requests?state=OPEN'
+    # Define the URL for creating a pull request
+    url = f'https://cedt-gct-bitbucket.nam.nsroot.net/bitbucket/rest/api/1.0/projects/{project}/repos/{repo_name}/pull-requests'
+
+    # Get the latest commit hashes for the base and compare branches
+    base_branch_url = f'https://cedt-gct-bitbucket.nam.nsroot.net/bitbucket/rest/api/1.0/projects/{project}/repos/{repo_name}/commits?until={base_branch}'
+    compare_branch_url = f'https://cedt-gct-bitbucket.nam.nsroot.net/bitbucket/rest/api/1.0/projects/{project}/repos/{repo_name}/commits?until={compare_branch}'
+
     try:
-        response = requests.get(url, auth=auth)
-        response.raise_for_status()  # Raise an exception for non-200 responses
+        base_response = requests.get(base_branch_url, auth=auth)
+        compare_response = requests.get(compare_branch_url, auth=auth)
+
+        base_response.raise_for_status()
+        compare_response.raise_for_status()
+
+        base_commit_hash = base_response.json()[0]['id']
+        compare_commit_hash = compare_response.json()[0]['id']
+
+        if base_commit_hash == compare_commit_hash:
+            logging.info("No changes between branches. No pull request created.")
+            return
+
     except requests.exceptions.RequestException as e:
-        logging.error(f"Failed to retrieve pull requests: {str(e)}", exc_info=True)
-        exit(1)
+        logging.error(f"Failed to retrieve commit hashes: {str(e)}", exc_info=True)
+        return
 
-    if response.status_code == 200:
-        pull_requests = response.json().get('values', [])
+    # Create the pull request payload
+    pull_request_payload = {
+        "title": "My Title",
+        "description": "My Description",
+        "destination": {
+            "branch": {
+                "name": base_branch
+            }
+        },
+        "source": {
+            "branch": {
+                "name": compare_branch
+            }
+        },
+        "reviewers": reviewers  # Add the list of reviewers here
+    }
 
-        # Iterate through pull requests and close or reject them
-        for pr in pull_requests:
-            pr_id = pr['id']
-            project = "CONSUMERAPI"
-            repo_name = pr["fromRef"]["repository"]["slug"]
-            version = pr["version"]
-            close_url = f'https://cedt-gct-bitbucket.nam.nsroot.net/bitbucket/rest/api/1.0/projects/{project}/repos/{repo_name}/pull-requests/{pr_id}/decline?version={version}&pullRequestId={pr_id}'
-            try:
-                response = requests.post(close_url, auth=auth, headers=headers)
-                response.raise_for_status()  # Raise an exception for non-204 responses
-            except requests.exceptions.RequestException as e:
-                logging.error(
-                    f"Failed to close pull request #{pr_id}: {str(e)}", exc_info=True)
-                continue  # Continue to the next pull request
+    # Send the POST request to create the pull request
+    try:
+        response = requests.post(url, json=pull_request_payload, auth=auth, headers=headers)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Failed to create pull request: {str(e)}", exc_info=True)
+        return
 
-            if response.status_code == 200:
-                logging.info(
-                    f"Closed pull request #{pr_id}")
-            else:
-                msg = response.json().get("errors", {}).get("message", "Unknown error")
-                logging.error(
-                    f"Failed to close pull request #{pr_id}: {response.status_code} - {msg}", exc_info=True)
+    if response.status_code == 201:
+        logging.info("Pull request created successfully.")
     else:
-        logging.error(
-            f"Failed to retrieve pull requests: {response.status_code}", exc_info=True)
+        logging.error(f"Failed to create pull request: {response.status_code} - {response.text}", exc_info=True)
 
     # Close the log file
     logging.shutdown()
 
-# Call the function to execute the code
-close_open_pull_requests()
+# Example usage:
+username = 'your_username'
+password = 'your_password'
+project = 'your_project'
+repo_name = 'your_repository'
+base_branch = 'staging'
+compare_branch = 'my-feature-branch'
+reviewers = ["reviewer1", "reviewer2"]  # Add the list of reviewers here
+
+create_pull_request(username, password, project, repo_name, base_branch, compare_branch, reviewers)
