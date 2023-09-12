f62fc2b69893e2d49b8cf5d421e6b9cc5fd0a3c1
Manoj Kumar <71879662+manojxk@users.noreply.github.com>
2023-09-12 19:10:54 +0530

    Update Python.py

--- Python.py
+++ Python.py
@@ -1,1 +1,25 @@
-https://cedt-gct-bitbucket.nam.nsroot.net/bitbucket/rest/api/1.0/xsrf-token
+import requests
+
+url = "https://cedt-gct-bitbucket.nam.nsroot.net/bitbucket/rest/api/1.0/dashboard/pull-requests"
+all_items = []
+
+while url:
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        items = data.get("values", [])
+        all_items.extend(items)
+        
+        # Check if there's a link to the next page in the response headers
+        next_url = data.get("next")
+        
+        if next_url:
+            url = next_url
+        else:
+            url = None  # No more pages, exit the loop
+    else:
+        print(f"Failed to retrieve data: {response.status_code}")
+        break
+
+# Now, all_items contains all the items from all pages
